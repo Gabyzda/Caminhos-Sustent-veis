41f8d2b801dfe0061a585b4e0c13f61780a579a4
Gabriela Zapata <gabyzda@gmail.com>
2023-05-11 14:57:02 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Caminhos Sustentáveis</a>
+      </header>
+      <main>
+        lista de serviços
+      </main>
+    </div>
+  );
+}
+
+export default App;
