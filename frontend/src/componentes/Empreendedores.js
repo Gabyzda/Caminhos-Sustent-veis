e38cc991d2737b7248b40a30caa2ee55e2888d5e
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-05-19 21:07:57 -0300

    Criação do componentes Emprendedores

--- frontend/src/componentes/Empreendedores.js
+++ frontend/src/componentes/Empreendedores.js
@@ -1,23 +1,44 @@
-import React from "react";
-/*import SobreNos from "./SobreNos";*/
-import { Link } from 'react-router-dom';
-/*import { useState } from "react";*/
-import '../Apprutas.css';
+import React from 'react';
+import { useNavigate, NavLink, Outlet } from 'react-router-dom';
 
-export default function Empreendedores() {
-  return (
-    <div>
-      <form className="presenta_css">
-        <div>
-          <h3> Caminhos Sustentáveis </h3>
-          <h1 className="centrasobrenos">
-            Pagina Emprendedores En construccion
-          </h1>
-          <Link to="/" style={{ textDecoration: 'none', color: "red" }} > ..[ RETORNAR A PÁGINA INICIAL ].. </Link>
+const Empreendedores = () => {
+
+    const navigate = useNavigate()
+
+    const goEmprendeproduto = () => {
+        navigate("/Empreendedores/Emprendeproduto")
+    }
+    const goEmprendeserviço = () => {
+        navigate("/Empreendedores/Emprendeserviço")
+    }
+    
+    return (
+        <div >
+            <hr />
+            <div className="App-headerdos">
+                <h3>Categoria de Empreendedores</h3>
+                <nav className="oldos_modulo">
+                    <button onMouseOver={goEmprendeproduto}>
+                        <NavLink to="/Empreendedores/Emprendeproduto" style={({ isActive }) =>
+                            (isActive ? { color: 'red' } : { color: 'blue' })} >
+                            PRODUTO
+                        </NavLink>
+                    </button>
+                    <button onMouseOver={goEmprendeserviço}>
+                        <NavLink to="/Empreendedores/Emprendeserviço" style={({ isActive }) =>
+                            (isActive ? { color: 'red' } : { color: 'blue' })}>
+                            SERVIÇOS
+                        </NavLink>
+                    </button>
+                   
+                </nav>
+            </div>
+            <hr />
+            <Outlet />
         </div>
-      </form>
-    </div>
-  );
+    )
 }
+
+export default Empreendedores;
 
  
--- frontend/src/componentes/Emprendeserviço.js
+++ frontend/src/componentes/Emprendeserviço.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 /*import { useState } from "react";*/
 import '../Apprutas.css';
 
-export default function Empreendedores() {
+export default function Registroemprendedorserv() {
   return (
     <div>
       <form className="presenta_css">
         <div>
           <h3> Caminhos Sustentáveis </h3>
           <h1 className="centrasobrenos">
-            Pagina SobreNos En construccion </h1>
+            Pagina Registro do emprededor de Servicos </h1>
           <Link to="/" style={{ textDecoration: 'none', color: "red" }} > ..[ RETORNAR A PÁGINA INICIAL ].. </Link>
         </div>
       </form>
