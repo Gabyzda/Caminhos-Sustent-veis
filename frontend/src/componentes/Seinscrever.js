c542bd8becc6a28ad7489d64dbcc679079befde4
Andres Reinoza <77939125+andyreinozaa@users.noreply.github.com>
2023-05-24 10:08:14 -0300

    Tradução dos placeholders Seinscrever.js

--- frontend/src/componentes/Seinscrever.js
+++ frontend/src/componentes/Seinscrever.js
@@ -22,12 +22,12 @@ export default function Seinscrever() {
       <div className="presenta_css">
           <h3> Formulario de cadastro  </h3>
           <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
+            <label>Nome</label>
             <input className='controls' type="text" name="user_name"  placeholder='Pedro Perez'/>
             <label>Email</label>
             <input className='controls' type="email" name="user_email" placeholder='Pedro@gmail.com'/>
-            <label>Message</label>
-            <textarea name="message" className='controls'  id= "message" placeholder='Escribe el nombre de tu negocio, Descripción del emprendimiento, categoría elegida (ej: alimentación, moda, tecnología, etc.):
+            <label>Mensagem </label>
+            <textarea name="message" className='controls'  id= "message" placeholder='Escreva o nome de sua empresa, a descrição do empreendimento e a categoria escolhida (por exemplo, alimentos, moda, tecnologia etc):
 Dirección:' />
             <input className='button' type="submit" value="Cadastrarse" />
           </form>
