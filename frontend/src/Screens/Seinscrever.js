fd6309c77600897868f63fe9a9ea5b655aa89edb
Gabriela Zapata <gabyzda@gmail.com>
2023-06-01 02:27:43 -0300

    -Criado form inscrição: -Add Email e Senha -Add Buttom Entrar

--- frontend/src/Screens/Seinscrever.js
+++ frontend/src/Screens/Seinscrever.js
@@ -1,7 +1,44 @@
-import React, { useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Helmet } from 'react-helmet-async';
+
+export default function SeInscrever() {
+  const { search } = useLocation();
+  const redirectInUrl = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInUrl ? redirectInUrl : '/';
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Inscrever-se</title>
+      </Helmet>
+      <h1 className="my-3">Inscrever-se</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" required />
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Entrar</Button>
+        </div>
+        <div className="mb-3">
+          Novo usuario?{' '}
+          <Link to={`/signup?redirect=${redirect}`}>Crie sua conta</Link>
+        </div>
+      </Form>
+    </Container>
+  );
+}
+
+
+
+/*import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-/*import SobreNos from "./SobreNos";*/
-/*import { useState } from "react";*/
 import { Helmet } from 'react-helmet-async';
 
 export default function Seinscrever() {
@@ -36,4 +73,4 @@ Dirección:' />
       </div>
     </div>
   );
-}
+}*/
